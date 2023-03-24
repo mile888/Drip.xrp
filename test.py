c28936bcfbe9cb6adfcfbf2b28f13cd7cfd45ebb
mile888 <mitrovich888@gmail.com>
2023-03-24 20:33:26 +0100

    mile

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
-import json
+# import json
 
-with open("config.json") as json_data_file:
-    data = json.load(json_data_file)
-print(data)
+# with open("config.json") as json_data_file:
+#     data = json.load(json_data_file)
+# print(data)
 
-event_1 = data["event_1"]
-print(event_1)
+# event_1 = data["event_1"]
+# print(event_1)
 
-for i in data:
-    print(data[f'{i}']["name"])
+# for i in data:
+#     print(data[f'{i}']["name"])
+
+products = {
+    'megatutorial': {
+        'name': 'The Flask Mega-Tutorial',
+        'price': 3900,
+    },
+    'support': {
+        'name': 'Python 1:1 support',
+        'price': 20000,
+        'per': 'hour',
+    },
+}
+
+for id in products:
+     print(id)
+     print(products[id])
+     
--- web_app.py
+++ web_app.py
@@ -6,86 +6,123 @@ from xrpl.models.transactions import Payment
 from xrpl.utils import xrp_to_drops
 from xrpl.transaction import autofill
 
+import json
+
+with open("config.json") as json_data_file:
+    events = json.load(json_data_file)
+
 
 
 JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
 
-ticket_store_address = 'rs6H4yR3kEBuEXXNQiYqAfg4NZuQUSppud'
-buyer = 'rsdKYDzru1BAwF8RehAxfBEWLafajE3crm'
+# ticket_store_address = 'rs6H4yR3kEBuEXXNQiYqAfg4NZuQUSppud'
+# buyer = 'rsdKYDzru1BAwF8RehAxfBEWLafajE3crm'
 
 
-tickets = {
-    'econom': {
-        'name' : 'Econom',
-        'number' : 70,
-        'price': 10000,
-    },
-    'VIP': {
-        'name' : 'VIP',
-        'number' : 15,
-        'price': 20000,
-    },
-}
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html', tickets=tickets)
+def home():
+    return render_template('home.html', event=events)
 
-@app.route('/success')
-def success():
-   return f'You succesfully payed a ticket'
+@app.route('/event', methods = ['POST', 'GET'])
+def order():
+    global id_web, client
+    if request.method == 'POST':
+        id_web = request.form.get("id")
+        num_of_ticket = request.form.get("number")
+        print(num_of_ticket)
+        client = JsonRpcClient(JSON_RPC_URL)
+        return render_template('payment.html', number=num_of_ticket)
+    
 
-
-@app.route('/order/econom', methods = ['POST', 'GET'])
-def order_econom():
-   global client
-   if request.method == 'POST':
-      client = JsonRpcClient(JSON_RPC_URL)
-      return render_template('payment_eco.html')
-#    else:
-#       user = request.args.get('nm')
-#       return redirect(url_for('success',name = user))
-   
-
-@app.route('/order/VIP', methods = ['POST', 'GET'])
-def order_vip():
-   if request.method == 'POST':
-      client = JsonRpcClient(JSON_RPC_URL)
-      return render_template('payment_vip.html')
-   
-
-@app.route('/order/econom/payed', methods = ['POST', 'GET'])
-def payment_econom():
+@app.route('/event/payed', methods = ['POST', 'GET'])
+def payment():
     if request.method == 'POST':
         address = request.form['address']
         email = request.form['email']
+
+        events[id_web]["number"] = events[id_web]["number"] - 1
+
+        with open("config.json", "w") as jsonFile:
+            json.dump(events, jsonFile)
         
         payment = Payment(
                 account=address,
-                amount=xrp_to_drops(tickets['econom']['price']),
-                destination=ticket_store_address)
+                amount=xrp_to_drops(events[id_web]["price"]),
+                destination=events[id_web]["url_account"])
         
         payment_done = autofill(payment, client)
         return render_template('success.html')
-        #return redirect(url_for('success'))
+    
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/success')
+# def success():
+#    return f'You succesfully payed a ticket'
+
+
+# @app.route('/order/econom', methods = ['POST', 'GET'])
+# def order_econom():
+#    global client
+#    if request.method == 'POST':
+#       client = JsonRpcClient(JSON_RPC_URL)
+#       return render_template('payment_eco.html')
+# #    else:
+# #       user = request.args.get('nm')
+# #       return redirect(url_for('success',name = user))
+   
+
+# @app.route('/order/VIP', methods = ['POST', 'GET'])
+# def order_vip():
+#    if request.method == 'POST':
+#       client = JsonRpcClient(JSON_RPC_URL)
+#       return render_template('payment_vip.html')
+   
+
+# @app.route('/order/econom/payed', methods = ['POST', 'GET'])
+# def payment_econom():
+#     if request.method == 'POST':
+#         address = request.form['address']
+#         email = request.form['email']
+        
+#         payment = Payment(
+#                 account=address,
+#                 amount=xrp_to_drops(tickets['econom']['price']),
+#                 destination=ticket_store_address)
+        
+#         payment_done = autofill(payment, client)
+#         return render_template('success.html')
+#         #return redirect(url_for('success'))
     
     
-@app.route('/order/VIP/payed', methods = ['POST', 'GET'])
-def payment_vip():
-    if request.method == 'POST':
-        address = request.form['address']
-        email = request.form['email']
+# @app.route('/order/VIP/payed', methods = ['POST', 'GET'])
+# def payment_vip():
+#     if request.method == 'POST':
+#         address = request.form['address']
+#         email = request.form['email']
         
-        payment = Payment(
-                account=address,
-                amount=xrp_to_drops(tickets['VIP']['price']),
-                destination=ticket_store_address)
+#         payment = Payment(
+#                 account=address,
+#                 amount=xrp_to_drops(tickets['VIP']['price']),
+#                 destination=ticket_store_address)
         
-        payment_done = autofill(payment, client)
-        return render_template('success.html')
-        #return redirect(url_for('success'))
+#         payment_done = autofill(payment, client)
+#         return render_template('success.html')
+#         #return redirect(url_for('success'))
 
 
 
